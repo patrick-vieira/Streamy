f96ab33dc40ba88cb37def16aa49ef8735333311
patrick-vieira <pdsv88@gmail.com>
2018-12-22 17:44:15 +0000

    first again

---
+++ client-app/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+const store = createStore(reducers, enhancer);
+
+const AppWhitProvider = () => {
+  return (
+    <Provider store={store} >
+      <App />
+    </Provider>)
+}
+
+
+ReactDOM.render(AppWhitProvider(), document.getElementById('root'));
+
